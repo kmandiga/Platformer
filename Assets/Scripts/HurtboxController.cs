897f55f06e3f9b6f2926b01baabde563fa3c2e0c
kmandiga <kmandiga@uh.edu>
2018-10-25 16:11:37 -0500

    Hitbox / Hurtbox
    -Added Hitbox & Hurtbox Prefabs
    -Changed collision matrix so that hitboxes and hurtboxes can only interact with each other
    -Boxes rotate with Sprite

---
+++ Assets/Scripts/HurtboxController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtboxController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Hurtbox OnTriggerEnter has been triggered");
+	}
+}
